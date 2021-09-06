16bc6587dbc20d347d0fd8b0f6bb78a0c539926f
linuxwt <tengwanginit@gmail.com>
2021-09-06 10:18:43 +0800

    添加cicd前后端测试文件

--- README.md
+++ README.md
@@ -25,6 +25,8 @@
 
 ## 五、[容器云项目-CICD组件配置二](https://linuxwt.com/rong-qi-yun-xiang-mu-2/)    
 
+## 六、[容器云项目-持续集成实践(一)](https://linuxwt.com/rong-qi-yun-xiang-mu-chi-xu-ji-cheng-shi-jian/)
+
 ## ps:本仓库所有配置是整个平台工具部署配置好后的完整配置，README.md里的每一篇博客文章是重现整个部署过程
 ## 所以很多配置是不完整的，需要将所有文章里的部署配置过程串联起来才可以得到仓库的完整配置
 
---
+++ node19/front/deploy.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+a=$(grep -Eh '"version"' package.json | awk -F ':' '{print $2}' | awk -F '"' '{print $2}')
+
+cat <<EOF> front.yaml
+apiVersion: apps/v1
+kind: Deployment 
+metadata: 
+  name: front
+  namespace: devops
+spec: 
+  selector:
+    matchLabels:
+      name: front
+  template: 
+    metadata: 
+      labels: 
+        name: front
+    spec: 
+      containers: 
+        - name: front
+          image: 10.0.0.19:30002/xj/front:$a
+          imagePullPolicy: IfNotPresent
+          ports: 
+            - containerPort: 8090
+          volumeMounts:
+            - name: tz-config
+              mountPath: /etc/localtime
+            - name: dockersock
+              mountPath: "/var/run/docker.sock"
+            - name: dockercli
+              mountPath: "/usr/bin/docker"
+            - name: dockerdaemon
+              mountPath: /etc/docker/daemon.json
+              subPath: daemon.json
+      imagePullSecrets:
+      - name: harbor-secret
+      nodeSelector:
+        app: devops
+      volumes:
+      - name: tz-config
+        hostPath:
+          path: /usr/share/zoneinfo/Asia/Shanghai
+      - name: dockerdaemon
+        configMap:
+          name: jenkins-configmap
+          items:
+          - key: daemon.json
+            path: daemon.json
+      - name: dockersock
+        hostPath:
+          path: /var/run/docker.sock
+      - name: dockercli
+        hostPath:
+          path: /usr/bin/docker
+---
+kind: Service
+apiVersion: v1
+metadata:
+  name: front-svc
+  namespace: devops
+  labels:
+    name: front
+  annotations:
+    description: Exposes Front  Service
+spec:
+  type: NodePort
+  selector:     
+    name: front    
+  ports:
+    - name: front
+      port: 8090
+      targetPort: 8090
+      nodePort: 30103
+EOF
+
+apk add sshpass
+sshpass -p xxxxx\@xxxx ssh -o StrictHostKeyChecking=no  10.0.0.19 'if [ -f front.yaml ];then rm -f front.yaml;fi'
+sshpass -p xxxx\@xxxx scp -o StrictHostKeyChecking=no  front.yaml 10.0.0.19:/root/deploy
+sshpass -p xxxx\@xxxx ssh -o StrictHostKeyChecking=no  10.0.0.19 'if [ $(kubectl get deploy -n devops | grep front | wc -l) -eq 1 ];then  kubectl delete -n devops deploy front && kubectl delete -n devops svc front-svc &&  kubectl create -f /root/deploy/front.yaml;else kubectl create -f /root/deploy/front.yaml;fi'
